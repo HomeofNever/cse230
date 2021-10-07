47b816d5cbc1389ba3f7d00d49b0bce828d1f1a7
NeverBehave <gayhub@never.pet>
2021-10-06 21:21:01 -0700

    add env

---
+++ shell.nix
@@ -0,0 +1,35 @@
+{ sources ? import ./nix/sources.nix }:     # import the sources
+with
+{ overlay = _: pkgs:
+    {
+      niv = (import sources.niv {}).niv;
+    };
+};
+let
+
+  pkgs = (import sources.nixpkgs) {
+    overlays = [ overlay ];
+    config.allowUnfree = true;
+    config.allowBroken = true;
+  };
+in pkgs.mkShell {
+  buildInputs = with pkgs; [
+    bash
+    vim
+
+    ghc
+    haskellPackages.stack
+    haskellPackages.elsa # Bring marked as broken, but usable. Probably elsa's test cases?
+   
+    nixStable
+
+    niv
+  ];
+
+  shellHook = ''
+    # Before run command
+    echo 'CSE230 - Course Env - Elsa - GHC'
+    echo 'Use `elsa file` to exec'
+    echo 'check makefile for more info'
+  '';
+}
